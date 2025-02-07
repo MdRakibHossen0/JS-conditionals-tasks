9deadeb5181abdb4f11ae0fbd128993f17fc93b1
MdRakibHossen0 <mdrakibhossencse@gmail.com>
2025-02-08 03:43:34 +0600

    added BMI calculator and Grade calculator and nested-friends

---
+++ task1.js
@@ -0,0 +1,18 @@
+/**
+ * programming hero js Conditional tasks
+ * TASK 1 - Free Drinks
+/**Free Drinks
+  - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burger = 650 ;
+
+if (burger > 500 ){
+    console.log('congratulation you get a free coke!')
+}
+else {
+    
+   burger = burger + 30 ;
+   console.log("You have to pay 30tk for coke.")
+}
