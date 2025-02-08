559176dd5f95003a5ac1f369c2b73d9ea4e99714
MdRakibHossen0 <mdrakibhossencse@gmail.com>
2025-02-08 19:49:45 +0600

    modify buse fare calculator

--- bus-fare-calculator.js
+++ bus-fare-calculator.js
@@ -7,23 +7,23 @@ Ticket fare Calculator
     - Otherwise Regular ticket fare 800 tk
 */
 
-let age = 25;
+let age = 60;
 let ticketPrice = 800;
 
+let isStudent = (ticketPrice * 50) / 100;
 
-if(age < 10 ){
-    console.log('Congratulation You get free ticket');
+if (age < 10) {
+  console.log("Congratulation You get free ticket");
+} else if (age > 10 && age <= 25) {
+  let isStudent = (ticketPrice * 50) / 100;
+  console.log(
+    "You get a 50% discount. After discount ticket far is: " +
+      (ticketPrice - isStudent)
+  );
+} else if (age >= 60) {
+  console.log(
+    "You get 15% Discount price: " + (ticketPrice - (ticketPrice * 15) / 100)
+  );
+} else {
+  console.log("Regular ticket price: " + ticketPrice);
 }
-else if( age > 10 && age <= 25){
-   let discount50 = (ticketPrice * 50) / 100;
-   console.log("You get a 50% discount. After discount ticket far is: " + (ticketPrice -discount50) );
-
-}
-
-else if(age >= 60){
-console.log('You get 15% Discount');
-}
-else{
-    console.log("Regular ticket price: " + ticketPrice );
-}
-
